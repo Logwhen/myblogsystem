21651dd7c002e78bb7ceb9c813dfe1635951280d
Logwhen <42775157@qq.com>
2019-11-18 21:12:05 +0800

    更新
    登录更新

--- src/main/java/com/blog/myblogsystem/MyblogsystemApplication.java
+++ src/main/java/com/blog/myblogsystem/MyblogsystemApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
+@MapperScan("com.blog.myblogsystem.dao")
 public class MyblogsystemApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/blog/myblogsystem/dao/UserMapper.java
+++ src/main/java/com/blog/myblogsystem/dao/UserMapper.java
@@ -7,26 +7,27 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
+@Mapper
 public interface UserMapper {
     //返回所有用户信息
-    @Select("select* from User")
+    @Select("select* from User;")
     List<User> getUserList();
     //按照ID查询用户
-    @Select("SELECT * FROM User WHERE ID = #{ID}")
+    @Select("SELECT * FROM User WHERE ID = #{ID};")
     public User findUserById(int ID);
     //按照名称查询用户
-    @Select("select *from user where name=#{name}")
+    @Select("select *from User where name=#{name};")
     public User findUserByName(String name);
     //按照名称删除用户
-    @Delete("delete from User where ID=#{ID}")
+    @Delete("delete from User where ID=#{ID};")
     int DeleteByID(int ID);
     //插入新用户
-    @Insert("insert into user(ID,name,password) values(#{ID},#{name},#{password})")
+    @Insert("insert into User(ID,name,password) values(#{ID},#{name},#{password});")
     int InsertUser(int ID, String name,String password);
     //根据ID更新用户信息
-    @Update("update user set ID=#{ID},name=#{name},password=#{password} where ID=#{ID}")
+    @Update("update User set ID=#{ID},name=#{name},password=#{password} where ID=#{ID};")
     int UpdateUser(int ID,String name,String password);
     //登录验证
-    @Select("select count(*) from User where name=#{name} and password=#{password}")
+    @Select("select count(*) from User where name=#{name} and password=#{password};")
     boolean VerifyUser(int ID,String password);
 }
