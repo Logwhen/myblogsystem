7957a45163670a7d1476f277996f9945be3e3a36
Logwhen <42775157@qq.com>
2019-11-18 16:18:43 +0800

    First Commit Test
    First Commit Test

---
+++ src/main/java/com/blog/myblogsystem/MyblogsystemApplication.java
@@ -0,0 +1,13 @@
+package com.blog.myblogsystem;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class MyblogsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyblogsystemApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/blog/myblogsystem/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.blog.myblogsystem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String Hello()
+    {
+        return "HelloWorld!";
+    }
+
+}
