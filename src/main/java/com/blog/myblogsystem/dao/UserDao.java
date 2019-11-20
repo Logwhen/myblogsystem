57b1e09d5bac4295b7447dda9e12997a674dbfa1
Logwhen <42775157@qq.com>
2019-11-20 22:20:13 +0800

    修改

---
+++ src/main/java/com/blog/myblogsystem/dao/UserDao.java
@@ -0,0 +1,18 @@
+package com.blog.myblogsystem.dao;
+
+import com.blog.myblogsystem.entity.User;
+import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+@Mapper
+public interface UserDao {
+    //返回所有用户信息
+    List<User> getUsers(User user);
+    void DeleteUser(User user);
+    void UpdateUser(User user);
+    void insertUser(User user);
+    List<User> getUser(User user);
+}
