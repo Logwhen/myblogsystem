1416be37e8c2b95bd612019ebf1357e1bab81a89
Logwhen <42775157@qq.com>
2019-11-21 14:11:11 +0800

    User

--- src/main/java/com/blog/myblogsystem/controller/SignUpController.java
+++ src/main/java/com/blog/myblogsystem/controller/SignUpController.java
@@ -11,34 +11,34 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
-import java.util.List;
 
 @RestController
-//用户登录接口
-public class LoginController {
+public class SignUpController {
     @Autowired
     private UserDao userDao;
     @Autowired
     private UserService userService;
-    @RequestMapping(value ="/login",method = RequestMethod.POST)
-    public Response Login(@RequestBody User user, HttpSession session)
+    @RequestMapping(value = "/SignUp",method = RequestMethod.POST)
+    public Response SignUp(@RequestBody User user)
     {
         Response response=new Response();
-        if(userService.Exist(user.getUsername()).getStatus()!="True")
+        if(userService.Exist(user.getUsername()).getStatus()=="True")
         {
-              return userService.Exist(user.getUsername());
+           response.setStatus("403");
+           response.setError("此用户名已注册，请重新输入");
+           return response;
         }
-        List<User> UserList=userDao.getUser(user);
-        if(UserList.size()==0)
+        try {
+            userDao.insertUser(user);
+        } catch (Exception e)
         {
-            response.setStatus("403");
-            response.setError("请输入正确的密码！");
+            e.printStackTrace();
+            response.setStatus("500");
+            response.setError("信息格式错误，请检查信息格式！");
             return response;
         }
-        session.setMaxInactiveInterval(-1);
-        session.setAttribute("id", UserList.get(0).getID());
         response.setStatus("200");
-        response.setError(session.getId());
+        response.setError("注册成功");
         return response;
     }
 }
--- src/main/java/com/blog/myblogsystem/controller/UserController.java
+++ src/main/java/com/blog/myblogsystem/controller/UserController.java
@@ -19,4 +19,5 @@ import java.util.List;
 public class UserController
 {
 
+
 }
