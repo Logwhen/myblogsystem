3b33ff4a8f6933ad8a85ac10457ce05960628ec0
Logwhen <42775157@qq.com>
2020-01-02 16:26:05 +0800

    评论，关注接口

---
+++ src/main/java/com/blog/myblogsystem/controller/CommentController.java
@@ -0,0 +1,43 @@
+package com.blog.myblogsystem.controller;
+
+import com.blog.myblogsystem.dao.CommentDao;
+import com.blog.myblogsystem.entity.Comment;
+import com.blog.myblogsystem.entity.Response;
+import com.blog.myblogsystem.service.SessionService;
+import org.apache.ibatis.annotations.Insert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@RestController
+public class CommentController {
+    @Autowired
+    CommentDao commentDao;
+    @Autowired
+   SessionService sessionService;
+    @RequestMapping(value = "comment/add",method = RequestMethod.POST)
+    Response addComment(@RequestBody Comment comment, HttpSession session)
+    {
+        Response response=new Response();
+        if (sessionService.authority(session).getStatus()!="200")
+        {
+            return sessionService.authority(session);
+        }
+        String userid=session.getAttribute("id").toString();
+        comment.setUserid(Integer.parseInt(userid));
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        SimpleDateFormat df1=new SimpleDateFormat("HH:mm:ss");
+        String curTime=df.format(new Date())+" "+df1.format(new Date());
+        comment.setTime(curTime);
+        commentDao.insertComment(comment);
+        response.setStatus("200");
+        response.setError("评论成功");
+        return response;
+    }
+}
---
+++ src/main/java/com/blog/myblogsystem/entity/Comment.java
@@ -0,0 +1,16 @@
+package com.blog.myblogsystem.entity;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter@Data
+public class Comment {
+    int commentid;
+    int blogid;
+    int userid;
+    int replyid;
+    String context;
+    String time;
+}
