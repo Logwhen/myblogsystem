e21d35cd1bffa1443cd91770ce1b60f8b50454a9
Logwhen <42775157@qq.com>
2020-01-02 15:34:06 +0800

    相册模块更新

--- src/main/java/com/blog/myblogsystem/controller/PictureController.java
+++ src/main/java/com/blog/myblogsystem/controller/PictureController.java
@@ -15,6 +15,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 @RestController
 public class PictureController {
@@ -25,7 +28,44 @@ public class PictureController {
     @RequestMapping(path = "picture/insert",method = RequestMethod.POST)
     Response insertPictures(@RequestBody String body, HttpSession session) throws IOException, ServletException {
         Response response=new Response();
+        Picture picture = new Picture();
+        picture.setUrl(body.toString());
+        String userid=session.getAttribute("id").toString();
+        picture.setUserid(Integer.parseInt(userid));
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        SimpleDateFormat df1=new SimpleDateFormat("HH:mm:ss");
+        String curTime=df.format(new Date())+" "+df1.format(new Date());
+        picture.setTime(curTime);
+        pictureDao.insertPicture(picture);
         System.out.println(body);
+        return response;
+    }
+    //获取当前用户的所有图片
+    @RequestMapping(path = "picture/get",method = RequestMethod.GET)
+    Response getUserPictures(@RequestBody Picture picture,HttpSession session)
+    {
+        Response response=new Response();
+        if (sessionService.authority(session).getStatus()!="200")
+        {
+            return sessionService.authority(session);
+        }
+        String userid=session.getAttribute("id").toString();
+        picture.setUserid(Integer.parseInt(userid));
+        List<Picture> pictureList=null;
+        pictureList=pictureDao.getuserPictures(picture);
+        response.setStatus("200");
+        response.setError("获取成功");
+        response.setResult(pictureList);
+        return  response;
+    }
+    @RequestMapping(path = "picture/delete",method = RequestMethod.DELETE)
+    Response deletePicture(@RequestBody Picture picture,HttpSession session)
+    {
+        Response response=new Response();
+        if (sessionService.authority(session).getStatus()!="200")
+        {
+            return sessionService.authority(session);
+        }
         return response;
     }
 }
